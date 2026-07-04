5212a3f3730bd09793e25e750b2e3c4575b6ca71
techMRB <techmrbcustom@gmail.com>
2026-07-04 16:13:05 +0530

    fix deploy error

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use("/api/users", userRoutes);
 app.use("/auth/api/", authRoutes);
 // to test the application or API
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to the application." });
+    res.json({ message: "HEllo from CI/CD pipeline" });
 });
 
 export default app;
